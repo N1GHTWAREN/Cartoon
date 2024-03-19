afed934614520bc087d94db8c1cb0b44df9c2226
N1GHTWAREN <bogdanesipov06@mail.ru>
2024-03-19 22:29:13 +0300

    fixed

--- main.py
+++ main.py
@@ -210,8 +210,7 @@ def on_click(message):
             card = all_cards[card_num]
             rarity_of_card = rarity_test[card_rarity]
             with open(f'{card_num}.jpg', 'rb') as photo:
-                if rolls != 0: bot.send_photo(message.chat.id, photo, f'Ты получил новую карту: {card[0]}\nГоды выпуска: {card[1]}\nСтрана: {card[2]}\nДвигатель: {card[3]}\nРедкость: {rarity_of_card[0]}\nРейтинг + {str(rarity_of_card[1])}')
-                elif (datetime.datetime.now() - last_time).seconds >= time_for_cooldown_lvls[cooldown_lvl - 1]: bot.send_photo(message.chat.id, photo, f'Ты получил новую карту: {card[0]}\nГоды выпуска: {card[1]}\nСтрана: {card[2]}\nДвигатель: {card[3]}\nРедкость: {rarity_of_card[0]}\nРейтинг + {str(rarity_of_card[1])}\n⏳ До следующей попытки {time_for_cooldown_lvls[cooldown_lvl - 1] // 3600} часа')
+                bot.send_photo(message.chat.id, photo, f'Ты получил новую карту: {card[0]}\nГоды выпуска: {card[1]}\nСтрана: {card[2]}\nДвигатель: {card[3]}\nРедкость: {rarity_of_card[0]}\nРейтинг + {str(rarity_of_card[1])}')
             cur.execute("UPDATE users SET number_of_cards = number_of_cards + 1 WHERE id = '%i'" % message.chat.id)
             conn.commit()
             cur.execute("UPDATE users SET rating = rating + '%i' WHERE id = '%i'" % (rarity_of_card[1], message.chat.id))
@@ -1310,7 +1309,7 @@ def callback_message(callback):
         else: cards[card] = 1
         cur.execute("UPDATE users SET cards = '%s' WHERE id = '%i'" % (json.dumps(cards), int(user[0])))
         conn.commit()
-        cur.execute("UPDATE users SET rating = rating + 1 WHERE id = '%i'" % int(user[0]))
+        cur.execute("UPDATE users SET rating = rating + 5000 WHERE id = '%i'" % int(user[0]))
         conn.commit()
         cur.execute("UPDATE users SET number_of_cards = number_of_cards + 1 WHERE id = '%i'" % int(user[0]))
         conn.commit()
